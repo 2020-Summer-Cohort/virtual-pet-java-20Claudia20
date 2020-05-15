ff6149581192c257522b4b70bcceafc7f109c1d8
20Claudia20 <leiaofalderran@gmail.com>
2020-05-15 14:27:33 -0400

    virtual pet class created

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,29 @@
+package virtual_pet;
+
+public class VirtualPet {
+    void tick(){
+
+    }
+
+    void getHunger(){
+
+    }
+
+    void getThirst(){
+
+
+    }
+    void getWaste(){
+
+    }
+
+    void getBoredom(){
+
+    }
+
+    void getPlay(){
+
+    }
+    void get
+
+}
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -1,9 +1,11 @@
 package virtual_pet;
 
 public class VirtualPetApplication {
-
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        //will have a loop
+        while (true){
+
+        }
     }
 
 }
